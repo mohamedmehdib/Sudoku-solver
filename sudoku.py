d4427817bff61c67554829d842ce0d337678aeea
Mohamed Mehdi Ben Haj Saleh <101983670+mohamedmehdib@users.noreply.github.com>
2024-10-28 21:46:46 +0100

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -2,89 +2,60 @@ class Solution:
     def solveSudoku(self, board):
         for i in range(9):
             for j in range(9):
-                if board[i][j] == "." :
-                    for k in range(1,10):
-                        if self.validSquare(str(k) , j , i ) and self.validHorizantally(board, str(k) , i) and self.validVertically(board , str(k) , j):
-                            board[i][j] = str(k)
-                            break
-                    if self.validSolved(board):
-                        return True
-
-    def validSolved(self,board):
-        for i in range(9):
-            for j in range(9):
                 if board[i][j] == ".":
-                    return False
-        return True
-    def validSquare(self,num,x,y):
-        if x < 3:
-            finX = 3
-        elif x < 6:
-            finX = 6
-        else:
-            finX = 9
-
-        if y < 3:
-            finY = 3
-        elif y < 6:
-            finY = 6
-        else:
-            finY = 9
-
-        miniBoard = [[" "," "," "],[" "," "," "],[" "," "," "]]
-
-        k = 0
-
-        for i in range(finY-3 , finY):
-            p = 0
-            for j in range(finX-3 , finX):
-                miniBoard[k][p]
-                p += 1
-            k += 1
-
-
-        for i in range(3):
-            for j in range(3):
-                if num == miniBoard[i][j]:
+                    for k in range(1, 10):
+                        num = str(k)
+                        if (self.validSquare(board, num, i, j) and 
+                            self.validHorizontally(board, num, i) and 
+                            self.validVertically(board, num, j)):
+                            board[i][j] = num
+                            if self.solveSudoku(board):
+                                return True
+                            board[i][j] = "."
                     return False
         return True
 
+    def validSquare(self, board, num, row, col):
+        startRow, startCol = 3 * (row // 3), 3 * (col // 3)
+        for i in range(startRow, startRow + 3):
+            for j in range(startCol, startCol + 3):
+                if board[i][j] == num:
+                    return False
+        return True
 
-    def validHorizantally(self,board,num,y):
-        for i in range(9):
-            if num == board[y][i]:
+    def validHorizontally(self, board, num, row):
+        for j in range(9):
+            if board[row][j] == num:
                 return False
         return True
 
-
-    def validVertically(self,board,num,x):
+    def validVertically(self, board, num, col):
         for i in range(9):
-            if num == board[i][x]:
+            if board[i][col] == num:
                 return False
         return True
 
-
-
-    def printBoard(self,board):
+    def printBoard(self, board):
         for i in range(9):
             for j in range(9):
-                if j%3 == 2 : 
-                    print(board[i][j] , end="|")
+                if j % 3 == 2:
+                    print(board[i][j], end="|")
                 else:
-                    print(board[i][j] , end=" ")
+                    print(board[i][j], end=" ")
             print()
-            if i%3 == 2 :
-                print("- "*9)
-
-
-
-
-
+            if i % 3 == 2:
+                print("- " * 9)
 
 sol = Solution()
-board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
-
+board = [["5","3",".",".","7",".",".",".","."],
+         ["6",".",".","1","9","5",".",".","."],
+         [".","9","8",".",".",".",".","6","."],
+         ["8",".",".",".","6",".",".",".","3"],
+         ["4",".",".","8",".","3",".",".","1"],
+         ["7",".",".",".","2",".",".",".","6"],
+         [".","6",".",".",".",".","2","8","."],
+         [".",".",".","4","1","9",".",".","5"],
+         [".",".",".",".","8",".",".","7","9"]]
 
 sol.solveSudoku(board)
-
 sol.printBoard(board)
